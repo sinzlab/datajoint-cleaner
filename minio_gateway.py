08a119dd9d890dc9c08dc91c47479cd75b59fb2f
Christoph Blessing <chris.blessing@protonmail.com>
2021-01-22 18:12:59 +0100

    Implement minio gateway

---
+++ minio_gateway.py
@@ -0,0 +1,51 @@
+"""Contains the MinIO gateway."""
+from typing import Dict, List, Set
+from uuid import UUID
+
+from .minio_facade import MinIOFacade
+
+
+class MinIOGateway:
+    """Gateway between the MinIO facade and the use-cases."""
+
+    def __init__(self, facade: MinIOFacade, bucket_name: str, location: str, schema_name: str) -> None:
+        """Initialize MinIOGateway."""
+        self.facade = facade
+        self.bucket_name = bucket_name
+        self.location = location
+        self.schema_name = schema_name
+        self._object_id_to_object_path_mapping: Dict[UUID, str] = {}
+
+    def get_object_ids(self) -> Set[UUID]:
+        """Get the object IDs."""
+        object_paths = self._get_object_paths()
+        object_ids = self._convert_object_paths_to_object_ids(object_paths)
+        self._add_object_ids_and_object_paths_to_mapping(object_ids, object_paths)
+        return set(object_ids)
+
+    def _get_object_paths(self) -> List[str]:
+        """Get the paths of MinIO objects."""
+        prefix = self.location + "/" + self.schema_name
+        object_paths = self.facade.get_object_paths(self.bucket_name, prefix=prefix)
+        return object_paths
+
+    def _convert_object_paths_to_object_ids(self, paths: List[str]) -> List[UUID]:
+        """Convert MinIO object paths to object IDs."""
+        object_names = [x.split("/")[-1] for x in paths]
+        object_names = [x.split(".")[0] for x in object_names]
+        object_ids = [UUID(x) for x in object_names]
+        return object_ids
+
+    def _add_object_ids_and_object_paths_to_mapping(self, object_ids: List[UUID], object_paths: List[str]) -> None:
+        for object_id, object_path in zip(object_ids, object_paths):
+            self._object_id_to_object_path_mapping[object_id] = object_path
+
+    def _convert_object_ids_to_object_paths(self, object_ids: Set[UUID]) -> Set[str]:
+        """Convert object IDs to MinIO object paths."""
+        object_paths = {self._object_id_to_object_path_mapping[x] for x in object_ids}
+        return object_paths
+
+    def delete_objects(self, object_ids: Set[UUID]) -> None:
+        """Delete the objects specified by the provided object IDs from the MinIO bucket."""
+        object_paths = self._convert_object_ids_to_object_paths(object_ids)
+        self.facade.remove_objects(self.bucket_name, object_paths)
