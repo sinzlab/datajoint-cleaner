91fb842dd172b205b1ff1c044a6a9fadcc156dc0
Christoph Blessing <chris.blessing@protonmail.com>
2021-02-02 16:46:15 +0100

    Move __init__ and __repr__ methods to ABC

--- dj_cleaner/use_cases/abstract.py
+++ dj_cleaner/use_cases/abstract.py
@@ -1,9 +1,16 @@
 """Contains the abstract base class all use-cases must inherit."""
 from abc import ABC, abstractmethod
 
+from .interfaces import AbstractDatabaseGateway, AbstractExternalGateway
+
 
 class UseCase(ABC):
     """Abstract base class for all use-cases."""
+
+    def __init__(self, db_gateway: AbstractDatabaseGateway, external_gateway: AbstractExternalGateway) -> None:
+        """Initialize Clean."""
+        self.db_gateway = db_gateway
+        self.external_gateway = external_gateway
 
     def __call__(self) -> None:
         """Execute the use-case."""
@@ -13,6 +20,6 @@ class UseCase(ABC):
     def _execute(self) -> None:
         """Execute the use-case."""
 
-    @abstractmethod
     def __repr__(self) -> str:
         """Return a string representation of the object."""
+        return f"{self.__class__.__name__}(db_gateway={self.db_gateway}, external_gateway={self.external_gateway})"
--- dj_cleaner/use_cases/clean.py
+++ dj_cleaner/use_cases/clean.py
@@ -1,17 +1,9 @@
 """Contains a simple prototype."""
-from __future__ import annotations
-
 from .abstract import UseCase
-from .interfaces import AbstractDatabaseGateway, AbstractExternalGateway
 
 
 class Clean(UseCase):  # pylint: disable=too-few-public-methods
     """Clean use-case."""
-
-    def __init__(self, db_gateway: AbstractDatabaseGateway, external_gateway: AbstractExternalGateway) -> None:
-        """Initialize Clean."""
-        self.db_gateway = db_gateway
-        self.external_gateway = external_gateway
 
     def _execute(self) -> None:
         """Clean up external objects."""
@@ -19,7 +11,3 @@ class Clean(UseCase):  # pylint: disable=too-few-public-methods
         db_object_ids = self.db_gateway.get_ids()
         to_be_deleted_object_ids = external_object_ids - db_object_ids
         self.external_gateway.delete_objects(to_be_deleted_object_ids)
-
-    def __repr__(self) -> str:
-        """Return a string representation of the object."""
-        return f"{self.__class__.__name__}(db_gateway={self.db_gateway}, external_gateway={self.external_gateway})"
