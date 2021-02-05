bb3b3427becc204033b94d91de82244048afb909
Christoph Blessing <chris.blessing@protonmail.com>
2021-02-05 18:19:26 +0100

    Test pymysql gateway

---
+++ tests/unit/adapters/test_pymysql_gateway.py
@@ -0,0 +1,66 @@
+from unittest.mock import MagicMock, create_autospec
+from uuid import UUID
+
+import pytest
+from dj_cleaner.adapters.pymysql_gateway import PyMySQLGateway
+from dj_cleaner.frameworks.pymysql_facade import PyMySQLFacade
+from dj_cleaner.use_cases.interfaces import AbstractDatabaseGateway
+
+
+def test_if_subclass_of_abstract_database_gateway():
+    assert issubclass(PyMySQLGateway, AbstractDatabaseGateway)
+
+
+@pytest.fixture
+def hashes():
+    hashes = [
+        b"\x11\xdc\x8b\xc5\xe9\x08c\xf9B\x1e.d\xb5E\x11r",
+        b"\xf0\xe6g\x13%\n\x90\t\xb3\xabs\x13\xedw@&",
+        b"~gN\x937k\x82^\xeet\xfa\x8d\x7f\xae\x84>",
+    ]
+    return [{"hash": h} for h in hashes]
+
+
+@pytest.fixture
+def facade(hashes):
+    facade = create_autospec(PyMySQLFacade)
+    facade.execute.return_value = hashes
+    facade.__repr__ = MagicMock(return_value="Facade()")
+    return facade
+
+
+@pytest.fixture
+def store_name():
+    return "my-store"
+
+
+@pytest.fixture
+def config(store_name):
+    return {"store_name": store_name}
+
+
+@pytest.fixture
+def gateway(facade, config):
+    return PyMySQLGateway(facade=facade, config=config)
+
+
+def test_if_facade_is_stored_as_instance_attribute(gateway, facade):
+    assert gateway.facade is facade
+
+
+def test_if_config_is_stored_as_instance_attribute(gateway, config):
+    assert gateway.config is config
+
+
+def test_if_execute_method_of_facade_is_called_correctly_when_getting_ids(gateway, facade, store_name):
+    gateway.get_ids()
+    external_store_name = "~external_" + store_name
+    facade.execute.assert_called_once_with(f"SELECT `hash` from `{external_store_name}`")
+
+
+def test_if_correct_object_ids_are_returned(gateway, hashes):
+    assert gateway.get_ids() == {UUID(bytes=h["hash"]) for h in hashes}
+
+
+def test_repr(gateway, facade, config):
+    assert repr(gateway) == f"PyMySQLGateway(facade={facade}, config={config})"
