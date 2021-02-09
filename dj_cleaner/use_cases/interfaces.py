98b18a81b60f317862af279096c246672d634271
Christoph Blessing <chris.blessing@protonmail.com>
2021-02-09 11:37:33 +0100

    Remove __repr__ method from interface

--- dj_cleaner/use_cases/interfaces.py
+++ dj_cleaner/use_cases/interfaces.py
@@ -11,10 +11,6 @@ class AbstractDatabaseGateway(ABC):
     def get_ids(self) -> Set[UUID]:
         """Get the IDs of entities stored in the database."""
 
-    @abstractmethod
-    def __repr__(self) -> str:
-        """Return a string representation of the object."""
-
 
 class AbstractExternalGateway(ABC):
     """Defines the interface of the external gateway as expected by the use-cases."""
@@ -26,7 +22,3 @@ class AbstractExternalGateway(ABC):
     @abstractmethod
     def delete_objects(self, object_ids: Set[UUID]) -> None:
         """Delete the objects specified by the provided object IDs from the external store."""
-
-    @abstractmethod
-    def __repr__(self) -> str:
-        """Return a string representation of the object."""
