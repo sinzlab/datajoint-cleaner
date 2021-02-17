9616499ae4c8fc3b559ef2d9355e763080dac378
Christoph Blessing <chris.blessing@protonmail.com>
2021-02-17 17:32:52 +0100

    Add tests for abstract response model

--- tests/unit/use_cases/test_abstract_use_cases.py
+++ tests/unit/use_cases/test_abstract_use_cases.py
@@ -1,7 +1,7 @@
 from abc import ABC
 from dataclasses import is_dataclass
 
-from dj_cleaner.use_cases.abstract import AbstractRequestModel
+from dj_cleaner.use_cases.abstract import AbstractRequestModel, AbstractResponseModel
 
 
 class TestAbstractRequestModel:
@@ -13,3 +13,11 @@ class TestAbstractRequestModel:
 
     def test_if_annotations_are_correct(self):
         assert all(x in AbstractRequestModel.__annotations__ for x in ("db_config", "storage_config"))
+
+
+class TestAbstractResponseModel:
+    def test_if_dataclass(self):
+        assert is_dataclass(AbstractResponseModel)
+
+    def test_if_subclass_of_abc(self):
+        assert issubclass(AbstractResponseModel, ABC)
